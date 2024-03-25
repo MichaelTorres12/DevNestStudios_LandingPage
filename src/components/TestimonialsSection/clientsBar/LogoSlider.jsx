6b38b5a9a8b8a2a6faa93f226d18b29d2644e97f
MichaelTorres12 <ia.michaeltorrescallejas@gmail.com>
2024-03-24 19:07:52 -0600

    It is already donde the logos slider responsive problem

--- src/components/TestimonialsSection/clientsBar/LogoSlider.jsx
+++ src/components/TestimonialsSection/clientsBar/LogoSlider.jsx
@@ -1,23 +1,25 @@
 import'./logosSlider.css';
 
+
+
 const logos = [
     "/testimonials/brands/3m.svg",
     "/testimonials/brands/budweiser.svg",
-    "/testimonials/brands/buzzfeed.svg",
     "/testimonials/brands/forbes.svg",
     "/testimonials/brands/mrbeast.svg",
+    "/testimonials/brands/buzzfeed.svg",
 
   ];
 
 export const LogoSlider = () => {
     return (
         <div className="logos">
-          <div className="logos-slide hover:cursor-pointer ">
+          <div className="logos-slide hover:cursor-pointer md:inline-flex ">
             {logos.map((logo, index) => (
               <img key={index} src={logo} alt="Company Logo" />
             ))}
           </div>
-          <div className="logos-slide hover:cursor-pointer xsm:hidden">
+          <div className="logos-slide hover:cursor-pointer xsm:hidden lg:inline-flex ">
             {logos.map((logo, index) => (
               <img key={index} src={logo} alt="Company Logo" />
             ))}
