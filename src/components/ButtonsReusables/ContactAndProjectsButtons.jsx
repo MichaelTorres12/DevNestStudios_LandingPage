10524dab599b4a2e5bd05a3622055f3a1d8a5d1e
Michael Torres <138163445+MichaelTorres12@users.noreply.github.com>
2024-04-20 10:56:08 -0600

    Update ContactAndProjectsButtons.jsx

--- src/components/ButtonsReusables/ContactAndProjectsButtons.jsx
+++ src/components/ButtonsReusables/ContactAndProjectsButtons.jsx
@@ -12,11 +12,11 @@ const scrollToSection = (sectionId) => {
 export const ContactAndProjectsButtons = () => {
   return (
     <div className="flex flex-wrap gap-2 xsm:px-8 md:p-0">
-        <a href="" onClick={() => scrollToSection('ContactUs')} className="flex md:gap-3 xsm:gap-2 xsm:justify-center text-2xl font-semibold bg-[#1658F4] text-white px-4 py-2 rounded-3xl  hover:bg-[#081273] hover:shadow-2xl hover:gray-500/100 xsm:w-full xsm:m-2 md:w-auto hover:cursor-pointer">
+        <a onClick={() => scrollToSection('ContactUs')} className="flex md:gap-3 xsm:gap-2 xsm:justify-center text-2xl font-semibold bg-[#1658F4] text-white px-4 py-2 rounded-3xl  hover:bg-[#081273] hover:shadow-2xl hover:gray-500/100 xsm:w-full xsm:m-2 md:w-auto hover:cursor-pointer">
         Contáctarnos
         <img src="../../../whiteArrow.svg" alt="goArrowWhite" loading="lazy"/>
         </a>
-        <a href="" onClick={() => scrollToSection('Proyectos')} className="flex md:gap-3 xsm:gap-2 xsm:justify-center text-2xl border-solid border-2 border-indigo-600 font-semibold bg-[#FFFFFF] text-[#1658F4] px-4 py-2 rounded-3xl hover:shadow-2xl hover:gray-500/100 xsm:w-full xsm:m-2 md:w-auto hover:cursor-pointer">
+        <a onClick={() => scrollToSection('Proyectos')} className="flex md:gap-3 xsm:gap-2 xsm:justify-center text-2xl border-solid border-2 border-indigo-600 font-semibold bg-[#FFFFFF] text-[#1658F4] px-4 py-2 rounded-3xl hover:shadow-2xl hover:gray-500/100 xsm:w-full xsm:m-2 md:w-auto hover:cursor-pointer">
         Ver proyectos
         <img src="../../../blueArrow.svg" alt="goArrowWhite" loading="lazy"/>
         </a>
